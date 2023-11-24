70489923b76e3ae2d2309969b7f9b48ded7ebc22
alex02r <alex.giochi02@gmail.com>
2023-11-24 14:02:04 +0100

    [UP]Svolta possibile soluzione

---
+++ array_bonus/js/script.js
@@ -0,0 +1,9 @@
+/* 
+    SOLUZIONE
+    1 - creaiamo un array di lunghezza x
+    2 - creaiamo un secondo array di lunghezza y (dove y è minore di x)
+    3 - for che va fino alla lunghezza maggiore
+        4 - ? array y < array x
+            5 - aggiungiamo un elemento a array y [i]
+    3 - chiudiamo il ciclo
+*/
