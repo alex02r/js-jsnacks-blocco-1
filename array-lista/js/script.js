f9c3e9a9f360b71433dd156ab4acf42729a04377
alex02r <alex.giochi02@gmail.com>
2023-11-24 13:32:09 +0100

    [UP]Aggiungiamo la pesca nell'array

---
+++ array-lista/js/script.js
@@ -0,0 +1,4 @@
+let fruits = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
+//aggiungiamo all'array la pesca
+fruits.push('pesca');
+console.log(fruits);
