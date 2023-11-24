b57e0cc047d4faf502615c43b897b86c241b2e99
alex02r <alex.giochi02@gmail.com>
2023-11-24 13:43:40 +0100

    [UP]Svolta la soluzione del problema

---
+++ array_tail/js/script.js
@@ -0,0 +1,12 @@
+/* 
+    SOLUZIONE
+    1 - chiediamo all'utente da quanti elementi deve essere composto l'array
+    2 - inizializziamo il ciclo for con la lunghezza data
+        3 - generiamo un numero casuale da 1 a 100
+        4 - inseriamo il numero nell'array
+    2 - fine ciclo
+    5 - con la funzione sort() ribaltiamo l'array
+    6 - inizializziamo un cilo for che va fino a 5
+        7 - stampiamo array[i]
+    6 - fine ciclo 
+*/
