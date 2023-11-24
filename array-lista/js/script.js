3846d457cca2690a9794a4fb51776f0cde2851f6
alex02r <alex.giochi02@gmail.com>
2023-11-24 13:36:31 +0100

    [UP]Controllo se cocomero è presente nell' array

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -1,4 +1,20 @@
 let fruits = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
 //aggiungiamo all'array la pesca
 fruits.push('pesca');
-console.log(fruits);
+
+//controlliamo se nell'array è presente il cocomero
+//inizializiamo la variabile per dopo poter inviare il messaggio
+let flag = false;
+//inizializzo il ciclo for per la lettura dell'array
+for (let i = 0; i < fruits.length; i++) {
+    if (fruits[i] == 'cocomero') {
+        flag = true
+        console.log('Trovato! Devo solo preparare il cocktail.');
+    }
+    
+}
+
+//controlliamo se la lettura non ha portato risultati
+if (!flag) {
+    console.log('Oh no, devo uscire a comprare il cocomero!');
+}
